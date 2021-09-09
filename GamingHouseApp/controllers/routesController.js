0d4ccf9cc0f16307070bfe6bb93a1eea7d780f62
Elliot Arana Morales <elliotam360@gmail.com>
2021-09-09 09:52:39 -0500

    modificación al register para tomar los datos JSON correctos

--- GamingHouseApp/controllers/routesController.js
+++ GamingHouseApp/controllers/routesController.js
@@ -6,12 +6,6 @@ const controlador = {
         res.render('AppHome', {view: 'home', articles: jsonFile.read()}); /*index*/
     },
     productCart: (req, res) => {
-<<<<<<< HEAD
-        res.render("AppProductcart", {view: 'car'}); /*productCart*/
-    },
-    productDetail: (req, res) => {
-        res.render("AppProductDetails", {view: 'detail'}); /*productDetails*/
-=======
         res.render("productCart");
     },
     productDetail: (req, res) => {
@@ -19,13 +13,11 @@ const controlador = {
     },
     register: (req, res) => {
         res.sendFile(path.join(__dirname, '../views/register.html'));
->>>>>>> 240c0cbebbd100ff5a38c53f89934bc92b4c9870
     },
     newProduct: (req, res) => {
         res.render("AppNewProduct", {view: 'new'}); /*newProduct*/
     },
     editProduct: (req, res) => {
-<<<<<<< HEAD
         res.render("AppEditProduct", {view: 'edit'}); /*editProduct*/
     },
     login: (req, res) => {
@@ -33,10 +25,6 @@ const controlador = {
     },
     register: (req, res) => {
         res.render("AppRegister"); /*register*/
-=======
-        res.render("editProduct");
-
->>>>>>> 240c0cbebbd100ff5a38c53f89934bc92b4c9870
     }
 }
 
